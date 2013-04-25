ecfb54b7220c2ce7074a27b4a295debd75391291
Mike Patek <mpatek@gmail.com>
2013-04-25 13:57:38 -0400

    separating out combination generation.

--- drum_writer.py
+++ drum_writer.py
@@ -1,4 +1,5 @@
 import itertools
+import random
 
 
 def bit_voices_to_notes(bit_voices, note_value=4):
@@ -38,7 +39,7 @@ def bits_to_notes(bits, voice="sn", note_value=4):
 
 def drum_voice(varname, notes):
     """ Returns a string representation of a drum voice. """
-    return '{0} = \drummode {{ {1} }}'.format(varname, ' '.join(notes))
+    return '{0} = \\repeat volta 2 \drummode {{ {1} }}'.format(varname, ' '.join(notes))
 
 
 def drum_staff(voices, directives):
@@ -56,25 +57,26 @@ def int_to_binstr(x, n_notes):
     return str(bin(x)).replace('0b', '').zfill(n_notes)
 
 
-def drum_permutation_generator():
-    instruments = ['hh', 'sn', 'bd', 'hhp']
+def drum_permutation_generator(
+        combinations,
+        instruments,
+        n_notes=4,
+        note_value=4,
+        ):
+                
+    n_instr = len(instruments)
     voices = {
-            'One': {'instruments': [0, 1], 'varname': 'up'},
-            'Two': {'instruments': [2, 3], 'varname': 'down'},
+            'One': {'instruments': range(0, n_instr/2), 'varname': 'up'},
+            'Two': {'instruments': range(n_instr/2, n_instr), 'varname': 'down'},
             }
-    n_notes = 4
-    for i, comb in enumerate(
-            itertools.combinations_with_replacement(range(2 ** n_notes), len(instruments))
-            ):
+    for i, comb in enumerate(combinations):
         lines = []
         for voice in voices:
             instr_bs = {}
             for instr_idx in voices[voice]['instruments']:
                 instr = instruments[instr_idx]
                 instr_bs[instr] = int_to_binstr(comb[instr_idx], n_notes)
-            notes = bit_voices_to_notes(instr_bs, n_notes)
-            notes.extend(notes)  # x2
-            notes.extend(notes)  # x4
+            notes = bit_voices_to_notes(instr_bs, note_value)
             lines.append(drum_voice(voices[voice]['varname'], notes))
         lines.append(drum_staff(
             dict((v, k['varname']) for v, k in voices.items()),
@@ -83,5 +85,35 @@ def drum_permutation_generator():
         yield lines
 
 
-for x in drum_permutation_generator():
+def permutation_combinations(n_notes, n_instr, n_combinations=100, maxn_zeros=1):
+    n_seen = 0
+    for comb in itertools.combinations_with_replacement(
+            range(2 ** n_notes), n_instr,
+            ):
+        n_zeros = sum(1 for x in comb if x == 0)
+        if n_zeros > maxn_zeros:
+            continue
+        yield comb
+        n_seen += 1
+        if n_seen >= n_combinations:
+            break
+
+def random_combinations(n_notes, n_instr, n_combinations=100, maxn_zeros=1):
+    for i in range(n_combinations):
+        comb = [random.randint(0, 2 ** n_notes - 1) for x in range(n_instr)]
+        n_zeros = sum(1 for x in comb if x == 0)
+        if n_zeros > maxn_zeros:
+            continue
+        yield comb
+
+
+"""
+instruments = ['hh', 'sn', 'bd', 'hhp']
+for x in drum_permutation_generator(instruments, n_notes=16, note_value=16):
+    print "\n".join(x)
+"""
+instruments = ['hh', 'sn', 'bd', 'hhp']
+#combs = permutation_combinations(4, len(instruments), maxn_zeros=0)
+combs = random_combinations(4, len(instruments))
+for x in drum_permutation_generator(combs, instruments, n_notes=4, note_value=4):
     print "\n".join(x)
